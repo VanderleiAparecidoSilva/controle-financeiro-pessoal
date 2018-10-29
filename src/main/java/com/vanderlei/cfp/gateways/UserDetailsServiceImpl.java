97715aa06ad473d674441d80bbd2bc6ed158dfbd
Vanderlei Silva <vanderlei@gmail.com>
2018-10-29 18:56:58 -0300

    Ajuste google formatter

--- src/main/java/com/vanderlei/cfp/gateways/UserDetailsServiceImpl.java
+++ src/main/java/com/vanderlei/cfp/gateways/UserDetailsServiceImpl.java
@@ -14,13 +14,13 @@ import java.util.Optional;
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
-    @Autowired
-    public UsuarioRepository usuarioRepository;
+  @Autowired public UsuarioRepository usuarioRepository;
 
-    @Override
-    public UserDetails loadUserByUsername(final String email) throws UsernameNotFoundException {
-        Optional<Usuario> obj = usuarioRepository.findByEmail(email);
-        Usuario usuario = obj.orElseThrow(() -> new UsernameNotFoundException(email));
-        return new UsuarioSecurity(usuario.getId(), usuario.getEmail(), usuario.getSenha(), usuario.getPerfis());
-    }
+  @Override
+  public UserDetails loadUserByUsername(final String email) throws UsernameNotFoundException {
+    Optional<Usuario> obj = usuarioRepository.findByEmail(email);
+    Usuario usuario = obj.orElseThrow(() -> new UsernameNotFoundException(email));
+    return new UsuarioSecurity(
+        usuario.getId(), usuario.getEmail(), usuario.getSenha(), usuario.getPerfis());
+  }
 }
