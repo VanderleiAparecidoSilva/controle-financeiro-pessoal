dc3a385714ef2b3184fbbd9006907bba96d130cb
Vanderlei Silva <vanderlei@gmail.com>
2018-11-13 15:24:33 -0200

    Ajustes

--- src/main/java/com/vanderlei/cfp/gateways/converters/UsuarioConverter.java
+++ src/main/java/com/vanderlei/cfp/gateways/converters/UsuarioConverter.java
@@ -3,24 +3,16 @@ package com.vanderlei.cfp.gateways.converters;
 import com.vanderlei.cfp.entities.Usuario;
 import com.vanderlei.cfp.http.data.UsuarioDataContract;
 import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Profile;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
-@Profile("oauth-security")
 @Component
 public class UsuarioConverter implements Converter<UsuarioDataContract, Usuario> {
 
-  @Autowired private BCryptPasswordEncoder passwordEncoder;
-
-  public UsuarioConverter() {
-    this.passwordEncoder = new BCryptPasswordEncoder();
-  }
-
   @Override
   public Usuario convert(final UsuarioDataContract dataContract) {
+    BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
     Usuario obj = new Usuario();
     BeanUtils.copyProperties(dataContract, obj);
     obj.setSenha(passwordEncoder.encode(dataContract.getSenha()));
